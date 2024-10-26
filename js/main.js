e4b95a661e3c17b6595eedf9306db93ccbbae09c
Mladja <mladenlukic111@gmail.com>
2024-10-26 14:14:49 +0200

    Added animation for hero section text and image

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@ hamburgerIcon.addEventListener('click', () => {
         e.classList.toggle('active');
     });
 });
+
+// Hero section text and image animation while scrolling into viewport
+const elementsToShow = document.querySelectorAll('.fade-in, .text-part');
+
+const showElementsOnScroll = () => {
+    elementsToShow.forEach(element => {
+        const rect = element.getBoundingClientRect();
+        if (rect.top < window.innerHeight - 200) {
+            element.classList.add('visible');
+        }
+    });
+};
+
+window.addEventListener('scroll', showElementsOnScroll);
