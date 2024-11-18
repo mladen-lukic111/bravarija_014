3210c661e42adbe788d4b74df8b6038ddf351ef0
Mladja <mladenlukic111@gmail.com>
2024-11-18 15:07:55 +0100

    Added functionality for menu links. Code in the _global.scss optimizied.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,23 @@
 const hamburgerIcon = document.querySelector('.hamburger-menu-wrapper');
 const hamburgerMenu = document.querySelector('.mobile-navigation-links');
 const bars = hamburgerIcon.querySelectorAll('span');
-hamburgerIcon.addEventListener('click', () => {
+const mobileLinks= document.querySelectorAll('.mobile-navigation-links li');
+
+// Function for menu closing
+const closeMenu = () => {
     hamburgerMenu.classList.toggle('active');
     bars.forEach(e => {
         e.classList.toggle('active');
     });
+};
+
+hamburgerIcon.addEventListener('click', ()=> {
+    hamburgerMenu.classList.toggle('active');
+        bars.forEach(b => {
+            b.classList.toggle('active');
+        });
+});
+
+mobileLinks.forEach(ml => {
+    ml.addEventListener('click', closeMenu);
 });
